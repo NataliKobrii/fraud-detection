ed9c92457e15b32bd16c722e033e2fe15f0031b7
Nataliia Kobrii <natalikobrii@gmail.com>
2026-02-17 20:13:55 -0500

    feat(maadsbml): add supervised fraud training and holdout evaluation

---
+++ supervised/maads_service/sqlloads/oticsusers.sql
@@ -0,0 +1,67 @@
+/*M!999999\- enable the sandbox mode */ 
+-- MariaDB dump 10.19  Distrib 10.11.11-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: maadsbml
+-- ------------------------------------------------------
+-- Server version	10.11.11-MariaDB-0+deb12u1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `oticsusers`
+--
+
+DROP TABLE IF EXISTS `oticsusers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `oticsusers` (
+  `company` varchar(250) DEFAULT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `email` varchar(250) DEFAULT NULL,
+  `startdate` varchar(50) DEFAULT NULL,
+  `active` int(11) DEFAULT NULL,
+  `lastlogin` varchar(50) DEFAULT NULL,
+  `contact` varchar(50) DEFAULT NULL,
+  `phone` varchar(50) DEFAULT NULL,
+  `usertype` varchar(50) DEFAULT NULL,
+  `logo` varchar(350) DEFAULT NULL,
+  `logowidth` int(11) DEFAULT NULL,
+  `logoheight` int(11) DEFAULT NULL,
+  `country` varchar(150) DEFAULT NULL,
+  `ispro` int(11) DEFAULT NULL,
+  `address` varchar(500) DEFAULT NULL,
+  `token` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `oticsusers`
+--
+
+LOCK TABLES `oticsusers` WRITE;
+/*!40000 ALTER TABLE `oticsusers` DISABLE KEYS */;
+INSERT INTO `oticsusers` VALUES
+('Otics','admin','Admin2018!','support@otics.ca','2018-11-04',1,'2021-07-18  09:15:13','admin','','admin','relogosingle.png',70,70,'canada',1,'Toronto,ON','NULL');
+/*!40000 ALTER TABLE `oticsusers` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-02-18  0:50:00
